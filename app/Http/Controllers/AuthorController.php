0925b83c71ec9ce95395876ecb49522aef76739a
AwsAboud <aws.amin.aboud@gmail.com>
2024-02-22 19:07:24 +0300

    base code

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\AuthorResource;
+use App\Http\Resources\AuthorCollection;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(new AuthorCollection(Author::all()),
+        Response::HTTP_OK);
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        return new AuthorResource($author);
+    }
+}
